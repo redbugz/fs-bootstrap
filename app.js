5a5563edcde0331cde6914fd5af4533958fc47a3
Logan Allred <redbugz@gmail.com>
2012-05-06 06:45:30 -0600

    adding lots of missing files to see if it will deploy to heroku

--- app.js
+++ app.js
@@ -5,28 +5,66 @@
 
 var   express = require('express')
     , routes = require('./routes')
-    , cons = require('consolidate')
-    , hogan = require('hogan.js')
+//    , cons = require('consolidate')
+//    , hogan = require('hogan.js')
+    , hulk = require('hulk-hogan')
     , u = require('underscore')
     , fs = require('fs')
     , sessions = require('cookie-sessions')
     , path = require('path')
-    , app = express();
+    , httpProxy = require('http-proxy')
+    , app = module.exports = express.createServer();
 
 //Extend String.prototype with format function
 require('./lib/stringFormat');
 
+var apiProxy, routingProxy, hostListRegEx = /^\/(?:familytree|identity|reservation|authorities|ct|watch|discussion|sources|links|source-links|temple)\/.*/;
+routingProxy = new httpProxy.RoutingProxy();
+
+apiProxy = function(req, res, next) {
+  if (hostListRegEx.test(req.url)) {
+    console.log("  proxy request: "+req.url);
+    console.dir(req.session);
+    if (req.user) {
+      console.log("adding user sessionId=" + req.user.sessionId);
+      req.url = req.url+"?sessionId="+req.user.sessionId;
+    }
+    if (req.session && req.session.auth && req.session.auth.familysearch && req.session.auth.familysearch.user) {
+      console.dir(req.session.auth.familysearch.user);
+      console.log("adding session sessionId=" + req.session.auth.familysearch.user.sessionId);
+      req.url = req.url+"?sessionId="+req.session.auth.familysearch.user.sessionId;
+    }
+    return routingProxy.proxyRequest(req, res, {
+      host: process.env.PROXY_HOST || "www.dev.usys.org",
+      port: process.env.PROXY_PORT || 80,
+      https: process.env.PROXY_HTTPS || false
+    });
+  } else {
+    return next();
+  }
+};
+
 app.configure(function(){
-  app.engine('html', cons.hogan);
-  app.set('view engine', 'html');
-  app.set('views', __dirname + '/views');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(sessions({secret: '8bfa313308bda335b456de2f145a5d23d21fedaae555e701ae4cd613cea2e4ba'}));
+  app.use(apiProxy);
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view options', {layout: false});
+  app.set('view engine', 'html');
+  app.register('.html', hulk);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+//Setup authorization handling
+  require('./lib/auth')(app, {
+    name: "fs-bootstrap",
+    version: "0.1.0"
+    //point the `baseUrl` property at your production base url.  You would probably want
+    //to set the url as an ENV var and then read that var here.
+    //baseUrl: "http://www.example.com/app" OR process.env.APP_BASE_URL
+  });
   app.use(app.router);
 });
 
@@ -38,16 +76,13 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-//Setup authorization handling
-require('./lib/auth')(app, {});
-
 var users = [];
 users.push({ name: 'tobi' });
 users.push({ name: 'loki' });
 users.push({ name: 'jane' });
 
 app.get('/users', function(req, res){
-  console.log("before render");
+  console.log("before render users");
   res.render('users', {
     title: 'Users',
     users: users,
@@ -56,7 +91,7 @@ app.get('/users', function(req, res){
 });
 
 app.get('/hero', function(req, res){
-  console.log("before render");
+  console.log("before render hero");
   res.render('hero', {
     title: 'Hero',
     users: users,
@@ -73,10 +108,12 @@ app.get('/about', function(req, res){
 });
 
 app.get('/', function(req, res){
-  console.log("before render");
+  console.log("before render index, user:");
+  console.dir(req.user);
   res.render('index', {
     title: 'Welcome',
-    projectName: "FS-Bootstrap"
+    projectName: "FS-Bootstrap",
+    user: req.user
   });
 });
 
